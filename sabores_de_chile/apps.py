bc4788649cd1a7c23bfd307acb08bb5e51290410
alvaro725 <alvaro.olivares16@inacapmail.cl>
2025-10-20 19:51:32 -0300

    probando github

--- sabores_de_chile/apps.py
+++ sabores_de_chile/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SaboresDeChileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sabores_de_chiLe'
+    name = 'sabores_de_chile'
