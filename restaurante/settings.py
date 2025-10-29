d0c381ce17c8b2001103ae1de6f6f924186c51b5
alvaro725 <alvaro.olivares16@inacapmail.cl>
2025-10-29 10:45:45 -0300

    abc

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'mydb',      
         'USER': 'root',         
         'PASSWORD': '123456',  
-        'HOST': '127.0.0.1',  
+        'HOST': 'localhost',  
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
